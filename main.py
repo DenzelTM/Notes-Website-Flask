f0d1cef4a0e675ab7a26df4e64b82b74d441ee5d
Denzel <denelmusiyiwa@gmail.com>
2022-07-27 13:21:31 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__' : #mean only when we run the file, we are going to validate the line below.
+    app.run(debug=True) #this helps automatically running the web server for us when changes are made.
+    
---
+++ website/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET KEY'] = 'IJKDFJMNVDO LMVMFNCVCVD'
+
+#registering our blueprints
+
+    from .views import views
+    from .auth import  auth
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+
+    return app
