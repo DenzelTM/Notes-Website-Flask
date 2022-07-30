c054d348f6599e89d400807fcfeee947fe11850f
Denzel <denelmusiyiwa@gmail.com>
2022-07-30 17:44:12 +0200

    2nd Commit

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__' : #mean only when we run the file, we are going to validate the line below.
-    app.run(debug=True) #this helps automatically running the web server for us when changes are made.
+    app.run(debug=True) #this helps auto running the web server for us when changes are made.
     
